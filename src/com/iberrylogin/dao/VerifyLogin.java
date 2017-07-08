f498e9a7fdef0013399e3f27b411b424bc6df062
Unitech1 <Unitech1@Unitech10-PC>
2017-07-07 18:25:58 -0700

    this  is my loginservelet done by sravanthi

--- src/com/iberrylogin/dao/VerifyLogin.java
+++ src/com/iberrylogin/dao/VerifyLogin.java
@@ -4,13 +4,27 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.*;
 
 public class VerifyLogin {
     public static boolean validate(String name, String pass) {
-        //This program validates the user id and password
-    	boolean status = false;
+    	boolean status=false;
+    	
+    	try{
+    			Class.forName("com.mysql.jdbc.Driver");
+    	Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/sravanthi","root","root");
+    	PreparedStatement pst=con.prepareStatement("select * from user");
+    	ResultSet rs=pst.executeQuery();
+    	while(rs.next()){
+    	if(rs.getString(1).equals(name)&&rs.getString(2).equals(pass)){
+    		System.out.print(rs.getString(1));
+    	status=true;
+    	}
+    	}
+    	}
+    	catch(Exception e){
+    		
+    		e.printStackTrace();
+    	}
         
         return status;
     }
